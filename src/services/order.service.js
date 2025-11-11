ac79c44ba9ab336b822a65469adbaa52a92c704f
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-11-11 15:19:27 +0700

    add https://

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -126,8 +126,8 @@ class OrderService {
                     brand_name: 'My Shop',
                     landing_page: 'LOGIN',
                     user_action: 'PAY_NOW',
-                    return_url: `web-app-frontend-fvnpwp3j3-tuananh19122003s-projects.vercel.app/paypal-success?orderId=${order.id}`,
-                    cancel_url: `web-app-frontend-fvnpwp3j3-tuananh19122003s-projects.vercel.app/payment-fail?orderId=${order.id}`,
+                    return_url: `https://web-app-frontend-fvnpwp3j3-tuananh19122003s-projects.vercel.app/paypal-success?orderId=${order.id}`,
+                    cancel_url: `https://web-app-frontend-fvnpwp3j3-tuananh19122003s-projects.vercel.app/payment-fail?orderId=${order.id}`,
                 },
             });
 

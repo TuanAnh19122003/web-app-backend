6563fc4b653268eaf4dbba63574ad08cef577fce
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-12-17 13:07:45 +0700

    thay đổi link frontend

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -126,8 +126,8 @@ class OrderService {
                     brand_name: 'My Shop',
                     landing_page: 'LOGIN',
                     user_action: 'PAY_NOW',
-                    return_url: `http://localhost:3000/paypal-success?orderId=${order.id}`,
-                    cancel_url: `http://localhost:3000/payment-fail?orderId=${order.id}`,
+                    return_url: `${process.env.FRONTEND_URL}/paypal-success?orderId=${order.id}`,
+                    cancel_url: `${process.env.FRONTEND_URL}/payment-fail?orderId=${order.id}`,
                 },
             });
 
